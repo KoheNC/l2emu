23d924e136aed154e38cb1179e26498978a27ecb
lordrex34 <lordrex34@022a2b8f-ba9a-981b-c8ea-d3a9f610ac58>
2011-02-12 23:56:39 +0000

    Initial commits are finished, now you can use the pack..

---
+++ tools/build-all.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##############################################
+# Configure this, if you don't have 'svn' in the path!
+PATH=${PATH}:/home/lord_rex/maven/apache-maven-2.2.1/bin
+
+MAVEN_OPTS="-Xms64m -Xmx256m"
+
+# Configure this, if you don't have 'mvn' in the path!
+MAVEN="mvn"
+##############################################
+
+echo ""
+cd ..
+cd L2J-MMOCore
+$MAVEN clean:clean install -Dmaven.test.skip=true
+cd ..
+cd L2EmuProject-Commons
+$MAVEN clean:clean install -Dmaven.test.skip=true
+cd ..
+cd L2EmuProject-Login
+$MAVEN clean:clean assembly:assembly -Dmaven.test.skip=true
+cd ..
+cd L2EmuProject-Game
+$MAVEN clean:clean install assembly:assembly -Dmaven.test.skip=true
+cd ..
+cd L2EmuProject-DataPack
+$MAVEN clean:clean assembly:assembly -Dmaven.test.skip=true
+cd ..
+cd tools
+echo ""
+echo "Sources compiled, and dependencies installed to the local repository."
