cb838ab7acf50f62e45cef3eb05c20532ac28129
l0ngh0rn@l2emuproject.net <l0ngh0rn@l2emuproject.net@022a2b8f-ba9a-981b-c8ea-d3a9f610ac58>
2011-02-15 15:39:50 +0000

    trunk/L2EmuProject-DataPack: * Converted scripts/ai/individual/LastImperialTomb - Need test. (With the new engine quest)

--- L2EmuProject-DataPack/sql/updates/mainteance_2011_02_15.sql
+++ L2EmuProject-DataPack/sql/updates/mainteance_2011_02_15.sql
@@ -1,2 +1,3 @@
 UPDATE `character_quests` SET `name` = 'VanHalter' WHERE `name` = 'vanhalter';
 UPDATE `character_quests` SET `name` = 'Darnel' WHERE `name` = 'darnel';
+UPDATE `character_quests` SET `name` = 'LastImperialTomb' WHERE `name` = 'lastimperialtomb';
