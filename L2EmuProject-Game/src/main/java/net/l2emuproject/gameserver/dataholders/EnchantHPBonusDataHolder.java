d66d1a641f0a142777f096e66f8b73cb7a7d5f9c
intrepid@l2emuproject.net <intrepid@l2emuproject.net@022a2b8f-ba9a-981b-c8ea-d3a9f610ac58>
2011-04-07 03:08:56 +0000

    refactoring done now :D

--- L2EmuProject-Game/src/main/java/net/l2emuproject/gameserver/dataholders/EnchantHPBonusDataHolder.java
+++ L2EmuProject-Game/src/main/java/net/l2emuproject/gameserver/dataholders/EnchantHPBonusDataHolder.java
@@ -56,7 +56,7 @@ public final class EnchantHPBonusDataHolder
 		final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setValidating(true);
 		factory.setIgnoringComments(true);
-		final File file = new File(Config.DATAPACK_ROOT, "data/char_data/enchant/enchantHPBonus.xml");
+		final File file = new File(Config.DATAPACK_ROOT, "data/item_data/enchant/enchantHPBonus.xml");
 		Document doc = null;
 
 		try
